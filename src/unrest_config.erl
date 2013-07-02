401c5902a00d266aa45e6e8888c49c7ed5dff0ca
Dmitrii Dimandt <dmitrii.dimandt@klarna.com>
2013-07-02 16:28:11 +0200

    New config works, you can run the app and go to /. Lots of debug output

--- src/unrest_config.erl
+++ src/unrest_config.erl
@@ -26,46 +26,100 @@ get_config() ->
   File   = filename:join([code:priv_dir(unrest), "config.yml"]),
   Result = yamerl_constr:file(File),
   Mappings = case Result of
-               [_Document, [M]] -> M;
-               [[Map]]          -> Map
+               [_Document, M] -> M;
+               Map            -> Map
              end,
   io:format("MAPPINGS ~n~p~n~n~n", [Mappings]),
   Mappings.
 
 -spec get_dispatches(list()) -> cowboy_router:dispatch_rules().
-get_dispatches(Config) ->
-  get_dispatches(Config, []).
+get_dispatches(Config0) ->
+  {Flows, Config} = get_named_flows(Config0),
+  get_dispatches(Config, Flows, []).
 
--spec get_dispatches(list(), list()) -> cowboy_router:dispatch_rules().
-get_dispatches([], Acc) ->
+-spec get_dispatches(list(), list(), list()) -> cowboy_router:dispatch_rules().
+get_dispatches([], _, Acc) ->
   lists:reverse(Acc);
-get_dispatches([{Path, Options} | Rest], Acc) ->
-  Opts = [{list_to_binary(Key), handle_list(List)} || {Key, List} <- Options],
+get_dispatches([{Path, Options} | Rest], Flows, Acc) ->
+  Opts = handle_methods(Options, Flows),
   Dispatch = {Path, unrest_handler, Opts},
-  get_dispatches(Rest, [Dispatch | Acc]).
+  get_dispatches(Rest, Flows, [Dispatch | Acc]).
 
-%%
-%% @doc Whe might want to handle a request exclusively ourselves, so instead of
-%%      a list of options we pass in module name
-%%
--spec handle_list(proplists:proplist()) -> proplists:proplist();
-                 (string())             -> atom().
-handle_list([{_, _}|_] = List) ->
-  [transform(E) || E <- List];
-handle_list(List) when is_list(List) ->
-  list_to_atom(List).
+get_named_flows(Config0) ->
+  {_, Flows0} = lists:keyfind("__flows__", 1, Config0),
+  io:format("Flows ~p~n~n~n", [Flows0]),
+  Flows1 = expand_flows(Flows0),
+  io:format("Expanded Flows ~p~n~n~n", [Flows1]),
+  Config = lists:keydelete("__flows__", 1, Config0),
+  {Flows1, Config}.
 
+expand_flows(Flows) ->
+  expand_flows(Flows, []).
 
-transform({"__flow__", List}) ->
-  Flow = [{list_to_atom(K), list_to_atom(V)} || {K, V} <- List],
+expand_flows([], Acc) ->
+  lists:reverse(Acc);
+expand_flows([Flow0 | Rest], Acc) ->
+  Flow = expand_flow(Flow0, Rest, Acc),
+  expand_flows(Rest, [Flow | Acc]).
+
+expand_flow({Key0, [{_,_} | _] = Values0}, NonExpanded, Expanded) ->
+  Key    = list_to_binary(Key0),
+  Values = expand_flow_values(Values0, NonExpanded, Expanded),
+  {Key, Values};
+expand_flow({Key, Module}, _, _) ->
+  {list_to_binary(Key), list_to_atom(Module)}.
+
+expand_flow_values(Values, NonExpanded, Expanded) ->
+  lists:flatten([expand_flow_value(V, NonExpanded, Expanded) || V <- Values]).
+
+expand_flow_value({"__flow__", [Name | _] = Flows}, NonExpanded, Expanded)
+  when is_list(Name) ->
+  [get_flow_by_name(NamedFlow, NonExpanded, Expanded) || NamedFlow <- Flows];
+expand_flow_value({"__flow__", Name}, NonExpanded, Expanded) ->
+  get_flow_by_name(Name, NonExpanded, Expanded);
+expand_flow_value({Module, [Fun | _] = Funs}, _, _) when is_list(Fun) ->
+  [{list_to_atom(Module), list_to_atom(F)} || F <- Funs];
+expand_flow_value({Module, Fun}, _, _) ->
+  {list_to_atom(Module), list_to_atom(Fun)};
+expand_flow_value(F, _, _) ->
+  F.
+
+get_flow_by_name(Name, NonExpanded, Expanded) ->
+  case lists:keyfind(Name, 1, NonExpanded) of
+    {_, _} = NonExpandedFlow ->
+      {_, Flow} = expand_flow(NonExpandedFlow, NonExpanded, Expanded),
+      Flow;
+    _ -> case lists:keyfind(list_to_binary(Name), 1, Expanded) of
+           {_, ExpandedFlow} -> ExpandedFlow;
+           _                 -> error({flow_not_found, Name})
+         end
+  end.
+
+handle_methods(Methods, Flows) ->
+  [handle_method(Method, Flows) || Method <- Methods].
+
+handle_method({Method, List}, Flows) ->
+  case io_lib:printable_list(List) of
+    true  ->
+      {maybe_to_binary(Method), list_to_atom(List)};
+    false ->
+      {maybe_to_binary(Method), handle_options(List, Flows)}
+  end.
+
+handle_options(Options, Flows) ->
+  io:format("OPTIONS ~p~n~n~n", [Options]),
+  [handle_option(Option, Flows) || Option <- Options].
+
+handle_option({"__flow__", Flow0}, ExpandedFlows) ->
+  Flow = expand_flow_values(Flow0, [], ExpandedFlows),
   {<<"__flow__">>, Flow};
-transform({K, V}) ->
-  {transform(K), transform(V)};
-transform(S) when is_list(S) ->
-  list_to_binary(S);
-transform(V) ->
-  V.
+handle_option(O, _) ->
+  O.
 
+maybe_to_binary(V) ->
+  try list_to_binary(V)
+  catch _:_ -> V
+  end.
 
 %%% Local Variables:
 %%% erlang-indent-level: 2
